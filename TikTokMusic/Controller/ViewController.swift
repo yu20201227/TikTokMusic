0d1408487ca51d94f1bddbd9bb999eb3f4db81e0
yu20201227 <yasada32020@gmail.com>
2020-12-09 13:51:22 +0900

    いろいろ修正

--- TikTokMusic/Controller/ViewController.swift
+++ TikTokMusic/Controller/ViewController.swift
@@ -14,5 +14,10 @@ class ViewController: UIViewController {
 
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
 
 }
